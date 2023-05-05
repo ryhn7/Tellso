7d14c1641c2e2a4db44db45c19ab6fc67ce37824
Rayhan Illyas <rayhan205732@gmail.com>
2023-05-05 21:27:27 +0700

    UI detail activity

---
+++ app/src/main/java/com/example/tellso/detail/DetailStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tellso.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_story)
+    }
+}
