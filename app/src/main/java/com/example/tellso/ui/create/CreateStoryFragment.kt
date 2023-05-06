be39f72591b3b2fedc3ff48cd60c24871b404d3f
Rayhan Illyas <rayhan205732@gmail.com>
2023-05-07 02:13:32 +0700

    on progress setup camera

--- app/src/main/java/com/example/tellso/ui/create/CreateStoryFragment.kt
+++ app/src/main/java/com/example/tellso/ui/create/CreateStoryFragment.kt
@@ -1,41 +1,146 @@
 package com.example.tellso.ui.create
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.*
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
 import com.example.tellso.databinding.FragmentCreateStoryBinding
+import com.example.tellso.utils.uriToFile
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
+@AndroidEntryPoint
 class CreateStoryFragment : Fragment() {
 
     private var _binding: FragmentCreateStoryBinding? = null
+    private lateinit var currentPhotoPath: String
+
+    private var getFile: File? = null
+    private var token: String = ""
+
+    private val viewModel: CreateStoryViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val file = File(currentPhotoPath).also { getFile = it }
+            val os: OutputStream
+
+            // Rotate image to correct orientation
+            val bitmap = BitmapFactory.decodeFile(getFile?.path)
+            val exif = ExifInterface(currentPhotoPath)
+            val orientation: Int = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+
+            val rotatedBitmap: Bitmap = when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(bitmap, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> TransformationUtils.rotateImage(bitmap, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> TransformationUtils.rotateImage(bitmap, 270)
+                ExifInterface.ORIENTATION_NORMAL -> bitmap
+                else -> bitmap
+            }
+
+            // Convert rotated image to file
+            try {
+                os = FileOutputStream(file)
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os)
+                os.flush()
+                os.close()
+
+                getFile = file
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            binding.previewImageView.setImageBitmap(rotatedBitmap)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            uriToFile(selectedImg, requireContext()).also { getFile = it }
+
+            binding.previewImageView.setImageURI(selectedImg)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val createStoryViewModel =
-            ViewModelProvider(this)[CreateStoryViewModel::class.java]
 
         _binding = FragmentCreateStoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
-
-
-//        val textView: TextView = binding.textStory
-//        createStoryViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            launch {
+                viewModel.getAuthToken().collect { authToken ->
+                    if (!authToken.isNullOrEmpty()) token = authToken
+                }
+            }
+        }
+
+//        binding.cameraButton.setOnClickListener { startIntentCamera() }
+        binding.galerryButton.setOnClickListener { startIntentGallery() }
+    }
+
+    private fun startIntentGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+//    private fun startIntentCamera() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        intent.resolveActivity(packageManager)
+//
+//        MediaUtility.createTempFile(application).also {
+//            val photoUri = FileProvider.getUriForFile(
+//                this,
+//                "com.artworkspace.storyapp",
+//                it
+//            )
+//            currentPhotoPath = it.absolutePath
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+//            launcherIntentCamera.launch(intent)
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/tellso/ui/create/CreateStoryViewModel.kt
+++ app/src/main/java/com/example/tellso/ui/create/CreateStoryViewModel.kt
@@ -3,11 +3,27 @@ package com.example.tellso.ui.create
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tellso.data.AuthRepo
+import com.example.tellso.data.StoryRepo
+import com.example.tellso.data.remote.response.StoriesResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import javax.inject.Inject
 
-class CreateStoryViewModel : ViewModel() {
+@HiltViewModel
+class CreateStoryViewModel @Inject constructor(
+    private val authRepo: AuthRepo,
+    private val storyRepo: StoryRepo
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Create story Fragment"
-    }
-    val text: LiveData<String> = _text
+    fun getAuthToken(): Flow<String?> = authRepo.getAuthToken()
+
+    suspend fun uploadStory(
+        token: String,
+        file: MultipartBody.Part,
+        description: RequestBody
+    ): Flow<Result<StoriesResponse>> =
+        storyRepo.uploadStory(token, file, description)
 }
